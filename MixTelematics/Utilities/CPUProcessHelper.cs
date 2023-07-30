58dfda7cb8009f0bb6acb8f3738376b4616d0944
martinkleinbooi@gmail.com <jackdaniels@123>
2023-07-31 01:24:51 +0200

    - Added check to force the process to run the program with real time priority to improve performance.

---
+++ MixTelematics/Utilities/CPUProcessHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace MixTelematics.Utilities
+{
+    public class CPUProcessHelper
+    {
+        public static void IncreaseProcessPriorityToRealTime()
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            currentProcess.PriorityClass = ProcessPriorityClass.RealTime;
+        }
+    }
+}
